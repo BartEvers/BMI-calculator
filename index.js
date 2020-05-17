1165e2a8f9241d020751a7010de44532a75fbad0
Bart Evers <bart@gillz.nl>
2020-05-17 16:51:30 +0200

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 var weightInKg = 82;
 var heightInM = 1.79;
 
-console.log(weightInKg);
-console.log(heightInM);
+// The formula for BMI is: weight (kg) / (height (m) x height (m))
+var BMI = weightInKg / (heightInM * heightInM);
+
+console.log(`
+**************
+BMI CALCULATOR
+**************
+
+height: ${heightInM}
+weight: ${weightInKg}
+
+****************
+FACING THE FACTS
+****************
+
+Your BMI is ${Math.round(BMI)}
+
+A BMI under 18.5 is considered underweight
+A BMI above 25 is considered overweight
+`);

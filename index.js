53ae8ac6c119bcd563ccc591c0a16e6ecef5e32d
Bart Evers <bart@gillz.nl>
2020-05-17 17:12:59 +0200

    diet plan

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ var age = parseInt(process.argv[4]);
 // The formula for BMI is: weight (kg) / (height (m) x height (m))
 var BMI = weightInKg / (heightInM * heightInM);
 
-// Assumptions ideal BMI is 22.5
+// Assumption: ideal BMI is 22.5
 // The formula for idealWeight is 22.5 x height (m) x height (m)
-var idealWeight = 22.5 * heightInM * heightInM;
+var idealWeightKg = 22.5 * heightInM * heightInM;
 
 // The formula for Basal Metabolic Rate (BMR) is: 10 x weight (kg) + 6.25 x height (cm) - 5 x age
 var heightInCm = heightInM * 100;
@@ -17,6 +17,15 @@ var BMR = 10 * weightInKg + 6.25 * heightInCm - 5 * age;
 
 // Assumption: calories for a normal lifestyle is BMR * 1.4
 var dailyCalories = BMR * 1.4;
+
+// Assumption: This app is built for people who weigh too much
+var weightToLoseKg = weightInKg - idealWeightKg;
+
+// Assumption: we can lose 0.5 kg a week
+var dietWeeks = weightToLoseKg / 0.5;
+
+// Assumption: to lose 0.5 kg a week we need to cut calorie intake by 500 calories
+var dietCalories = dailyCalories - 500;
 
 console.log(`
 **************
@@ -31,6 +40,12 @@ FACING THE FACTS
 Your BMI is ${Math.round(BMI)}
 A BMI under 18.5 is considered underweight
 A BMI above 25 is considered overweight
-Your ideal weight is ${Math.round(idealWeight)} kg
+Your ideal weight is ${Math.round(idealWeightKg)} kg
 With a normal lifestyle you burn ${Math.round(dailyCalories)} calories a day
+**********
+DIET PLAN
+**********
+If you want to reach your ideal weight of ${Math.round(idealWeightKg)} kg:
+Eat ${Math.round(dietCalories)} calories a day
+For ${Math.round(dietWeeks)} weeks
 `);
